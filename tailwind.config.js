38633719af2ca0b55475248f4149fea5e03770c5
DevServer24 <developmentserver160@gmail.com>
2025-09-17 22:41:11 +0800

    added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-geist-sans)'],
+        mono: ['var(--font-geist-mono)'],
+      },
+    },
+  },
+  plugins: [],
+};
